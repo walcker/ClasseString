8f36297fed624acd23648700a7a344ee2467c0ee
Walcker Gomes <walckergomes@gmail.com>
2022-04-24 23:16:49 -0300

    Initial commit

---
+++ src/br/ufrn/imd/ClasseString.java
@@ -0,0 +1,12 @@
+package br.ufrn.imd;
+
+public class ClasseString {
+
+    public static void main(String[] args) {
+        System.out.println("abc");
+        String cde = "cde";
+        System.out.println("abc" + cde);
+        String c = "abc".substring(2,3);
+        String d = cde.substring(1, 2);
+    }
+}
